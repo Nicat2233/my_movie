b52f1d4656cb777bc2b234c77f0960f053dc8f3a
Nicat2233 <azerto2233@gmail.com>
2026-02-20 20:17:03 +0400

    d

---
+++ My-movie/src/main/java/com/movie/dea/dto/MovieDTO.java
@@ -0,0 +1,92 @@
+package com.movie.dea.dto;
+
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+public class MovieDTO {
+    private Integer id;
+
+    //    @Size(min = 5, max = 100, message = "Title must be 5-100 chars")
+    private String title;
+    //    @NotBlank(message = "Genre is required!")
+//    @Size(min = 5, max = 100, message = "Genre must be 5-100 chars")
+    private String genre;
+
+    //    @NotNull(message = "dont leave empty")
+    private LocalDate releaseDate;
+    //    @NotNull(message = "dont leave empty")
+//    @DecimalMin(value = "1.0", message = "Rating must be at least 0")
+//    @DecimalMax(value = "10.0", message = "Rating must be at most 10")
+    private Double rating;
+    //    @NotBlank(message = "Duration is required!")
+//    @Size(min = 2, max = 3, message = "Duration must be 2-3 chars") // 1000
+    private String duration;
+    //    @NotNull(message = "Shoudnt be null!")
+    private Integer directorId;
+    private String directorName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public Integer getDirectorId() {
+        return directorId;
+    }
+
+    public void setDirectorId(Integer directorId) {
+        this.directorId = directorId;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+}
